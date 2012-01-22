27684e3aaa70ee8b6485388ba1edd86d97453eb9
Chris Couzens <ccouzens@gmail.com>
2012-01-22 22:45:30 +0000

    Work in progress: implement albums of an artist

--- musiclibrary.py
+++ musiclibrary.py
@@ -5,7 +5,7 @@ from gi.repository import Gio
 from gi.repository import GObject
 from gi.repository import Tracker
 
-ALBUM_QUERY = """
+ARTIST_QUERY = """
 SELECT ?artist ?artist_name
 WHERE {
     ?album a nmm:MusicAlbum; 
@@ -17,6 +17,39 @@ WHERE {
 GROUP BY ?artist
 ORDER BY ?artist_name
 """
+
+def sparql_safe (string):
+    # is this enough?
+    # what is the best way to guard against sparql injection?
+    # Perhaps this? http://developer.gnome.org/libtracker-sparql/unstable/tracker-examples-builder.html
+    # I can't get my head around it though.
+    # 
+    # bug, should be using the builder as described above
+    return string.replace('"', '\\\"')
+
+def albums_query (artist):
+    # bug, should use the artist object, not the artist name
+    artist = sparql_safe(artist)
+    return """
+    SELECT ?album ?album_name
+    WHERE {
+        ?album a nmm:MusicAlbum; 
+            nmm:albumArtist [ nmm:artistName "%s" ].
+        # make sure the album isn't empty
+        ?song nmm:musicAlbum ?album .
+    }  
+    GROUP BY ?album
+    ORDER BY ?album_name
+    """ % (artist, )
+
+
+#
+#
+#
+# Handle expanding an artist
+#
+#
+#
 
 
 class MusicLibrary(GObject.Object, Peas.Activatable):
@@ -30,6 +63,7 @@ class MusicLibrary(GObject.Object, Peas.Activatable):
 
     def do_activate(self):
         self.totem = self.object
+        # bug, Tracker stuff should be async
         self.conn = Tracker.SparqlConnection.get (None)
         builder = Totem.plugin_load_interface ("musiclibrary", "musiclibrary.ui", True, self.totem.get_main_window (), self)
         container = builder.get_object ('root_window')
@@ -45,6 +79,6 @@ class MusicLibrary(GObject.Object, Peas.Activatable):
         self.totem.remove_sidebar_page ("musiclibrary")
 
     def populate_album_list (self):
-        cursor = self.conn.query (ALBUM_QUERY, None)
+        cursor = self.conn.query (ARTIST_QUERY, None)
         while cursor.next (None):
             self.library_tree_store.append(None, (cursor.get_string(1)[0],))
